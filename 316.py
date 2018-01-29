435787537eb4fa51d6c59fca9339b09181f4b5f5
Bo.Hoang <bo.hoang@anz.com>
2018-01-30 10:01:22 +1100

    update

---
+++ 316.py
@@ -0,0 +1,41 @@
+#316. Remove Duplicate Letters
+"""
+Given "bcabc"
+Return "abc"
+
+Given "cbacdcbc"
+Return "acdb"
+"""
+class Solution:
+
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        idx = {}        
+        print("s: ", s)
+
+        for i, ch in enumerate(s):
+            if ch not in idx:
+                idx[ch] = i
+            oldIdx = idx[ch]
+            oldLexOrder = ''.join([ ch for idx,ch in sorted((y,x) for x,y in idx.items()) ])
+            idx[ch] = i
+            newLexOrder = ''.join([ ch for idx,ch in sorted((y,x) for x,y in idx.items()) ])
+            
+            print( "old: ", oldLexOrder , "  new: ", newLexOrder)
+
+            if oldLexOrder != newLexOrder:
+                greedyLeft = self.removeDuplicateLetters( oldLexOrder + s[i+1:])
+                greedyRight = self.removeDuplicateLetters( newLexOrder + s[i+1:])
+                print( "greedyLeft: ", greedyLeft , "  greedyRight: ", greedyRight)
+                if greedyLeft < greedyRight:
+                    print("revert to ", oldLexOrder)
+                    idx[ch] = oldIdx  #revert
+
+
+        print("min-str:%s" % ''.join([ ch for key,ch in sorted((y,x) for x,y in idx.items())]) )
+        return ''.join([ ch for key,ch in sorted((y,x) for x,y in idx.items())])
+
+print("result: " , Solution().removeDuplicateLetters("bcabc"))
--- 401.py
+++ 401.py
@@ -1,17 +1,59 @@
+#88ms
 class Solution:
     def readBinaryWatch(self, num):
         """
         :type num: int
         :rtype: List[str]
         """
-        #10-bit
-        start = (0b0000001111111111 >> (10 - num))
-        while :
-            formatTime(start)
-            start <<=1
-         
+        self._results = []
+        self.helper( 0b0000000000, 10, num)
+
+        return self._results
+
+    def reject(self, ts):
+        return (ts & 0b1111000000) >= 0b1100000000 or (ts & 0b0000111111 > 59)
+
+
+    def outputTime(self, timebits):
+        if self.reject(timebits):
+            return
         
-    def formatTime(self, timebits):
-        hour = (timebits & 0b01111000000) >> 6
-        minitue = (timebits & 0xb0000111111)
-        print("%d:%02d"% (hour, minitue))
+        self._results.append('%d:%02d' % (timebits  >> 6, timebits & 0b0000111111))
+
+    def helper(self, fixedUpperBits, length, onBits):
+
+        if self.reject(fixedUpperBits):
+            return
+
+        if onBits == 1:
+            singleBitMask = 0b1;
+            for  i in range(length):
+                self.outputTime(fixedUpperBits | singleBitMask)
+                singleBitMask <<=1
+            return
+
+        elif onBits == length:
+
+            self.outputTime(fixedUpperBits | (2**onBits - 1))
+            return
+
+        else:
+            start = 0
+            for d in range( length-1 ):
+                if d < onBits:
+                    start |= 0b1
+                start <<= 1
+
+            self.outputTime( fixedUpperBits | start)
+
+            while start > 0:
+                newLength = length - onBits
+                start -= 0b1<< newLength
+                leftBits = onBits - bin(start).count('1')
+
+                if leftBits <= newLength:
+                    self.helper( fixedUpperBits | start, newLength,  leftBits )
+
+        return
+
+print(Solution().readBinaryWatch(5))
