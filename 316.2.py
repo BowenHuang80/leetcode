8eb1e4c3578e70fff54c3ba4fffe36c5826d19a4
biRain <neverforum@hotmail.com>
2018-03-12 20:53:05 -0400

    updated 316.2 316.loop

--- 316.2.py
+++ 316.2.py
@@ -1,20 +1,24 @@
 class Solution:
+    def __init__(self):
+        self.nest =''
 
     def removeDuplicateLetters(self, s):
         """
         :type s: str
         :rtype: str
         """
+
         charSet = sorted(set(s), reverse=True)
-        print("start: ", s, charSet)
+        print("start: ", s, self.nest) #charSet)
 
         for key, y in enumerate(charSet):
             idx1 = s.find(y)
             idx2 = s.find(y, idx1+1)
             print("y:", y, idx1, idx2)
             while idx1 != -1 and idx2 != -1: # found y, and not the last one
-             
+                """
                 idxZ = -1                   #idxZ = s.find( Z, idx1, idx2)            
+                
                 for Z in charSet[:key]:
                     idxZ = s.find(Z, idx1, idx2 )
                     if -1 != idxZ:
@@ -29,11 +33,14 @@ class Solution:
                     if -1 != idxX:
                         print("X:",X)
                         break  
-                
+                """
+
+                idxZ = [ idxZ for idxZ in [s.find(Z, idx1, idx2) for Z in charSet[:key]] if idxZ != -1]
+                idxX = [ idxX for idxX in [s.find(X, idx1, idx2) for X in charSet[key+1:]] if idxX != -1]
                 print(idxZ, idxX)
 
-                if idxZ != -1:
-                    if idxX == -1 or idxX > idxZ:
+                if len(idxZ) != 0:
+                    if len(idxX) == 0 or min(idxX) > max(idxZ):
                         #yZy #yZXy  => yZ
                         #remove_yZy(s, idx2)
                         s = s[0:idx2] + s[idx2:].replace(y, '')
@@ -41,8 +48,19 @@ class Solution:
                         break
                     else:
                         #yXZy => [XZy | y'X'Z] , where 'X' will be removed later
+                        self.nest += 'a'
                         greedy_XZy = self.removeDuplicateLetters( s[:idx1] + s[idx1+1:])
+                        self.nest = self.nest[:-1]
+
+                        if self.nest == 'a':
+                            print("greedy_XZy:", greedy_XZy)
+                            return
+
+                        self.nest += 'b'
                         greedy_yXZ = self.removeDuplicateLetters( s[:idx2] + s[idx2+1:])
+                        self.nest = self.nest[:-1]
+
+                        print(self.nest, greedy_XZy, greedy_yXZ)
                         if greedy_XZy < greedy_yXZ:
                             #remove_yXZy(s, idx1) # remove 1st y
                             s = s[:idx1] + s[idx1+1:]
@@ -55,7 +73,7 @@ class Solution:
                             s = s[:idx2] + s[idx2:].replace(y, '')
                             print("yXZy => XZy", s, y, idx1, idx2, idxX, idxZ)
                             break
-                elif idxX != -1:
+                elif len(idxX) != 0:
                     # yXy => Xy
                     #remove_yXy(s, idx1)
                     s = s[:idx1] + s[idx1+1:]
@@ -71,7 +89,6 @@ class Solution:
                     idx2 = s.find(y, idx1+1)
                     print("yy2", s, y, idx1, idx2, idxX, idxZ)
                     continue # while idx1 != -1
-                    
 
         return s
 print("result: ciorhsaebpunvdyktzfjlgwq" , Solution().removeDuplicateLetters("yioccqiorhtoslwlvfgzycahonecugtatbyphpuunwvaalcpndabyldkdtzfjlgwqk"))
@@ -82,3 +99,4 @@ print("result: hesitxyplovdqfkz" , Solution().removeDuplicateLetters("thesqtitxy
 #print("result: " , Solution().removeDuplicateLetters("ccacbacba"))
 #print("result: " , Solution().removeDuplicateLetters("ccacbaba"))
 #print("result: " , Solution().removeDuplicateLetters("baab"))
+
